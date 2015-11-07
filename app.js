ffbc80a5abca7bad588dfca5eb82c46c68ffc442
Philip <philipthov@gmail.com>
2015-11-07 10:43:35 -0800

    add express and post endpoint

--- app.js
+++ app.js
@@ -2,15 +2,57 @@ var Docker = require('dockerode');
 var fs = require('fs');
 var cfg = require('./config');
 
-//var docker1 = Docker();
-var docker1 = new Docker({
-						host: cfg.host_ip, 
-						port: 2376,
-						protocol: 'https',
-						ca: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/ca.pem'),
-	  					cert: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/cert.pem'),
-	  					key: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/key.pem')
-					}); //defaults to http
+var express = require('express');
+
+var app = express();
+
+
+app.get ('/', function(req, res){
+
+	res.send('<H1>hello! would you like to launch some container?</H1>')
+})
+
+app.post ('/launch', function(req,resp){
+	launchContainer();
+	resp.send('All good');
+})
+
+var server = app.listen(3000, function(){
+
+
+	console.log('listen to port 3000')
+})
+
+function launchContainer(){
+	//var docker1 = Docker();
+	var docker1 = new Docker({
+							host: cfg.host_ip, 
+							port: 2376,
+							protocol: 'https',
+							ca: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/ca.pem'),
+		  					cert: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/cert.pem'),
+		  					key: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/key.pem')
+						}); //defaults to http
+
+	var HostPort = 5033; //use for testing
+
+	docker1.createContainer({
+	  Image: cfg.imagename,
+	  "Env": ["APP_NAME=" + cfg.appname, "HOST_PORT=" + HostPort]
+	}, function(err, container) {
+		console.log("Host port : " + HostPort);
+		  container.start({
+		  	"ExposedPorts": {"3000/tcp": {}},
+		  	//uncomment if we want to provide host port instead of dynamically allocate
+			"PortBindings": {"3000/tcp":   [{}]}  //[{"HostPort": HostPort.toString()}]} //
+		  }, function(err, data) {
+		  	console.log('Container started : ' + data);
+		   // runExec(container);
+		  });
+	});
+}
+
+
 
 /**
  * Get env list from running container
@@ -35,20 +77,5 @@ function runExec(container) {
   });
 }
 
-var HostPort = 5033; //use for testing
 
-docker1.createContainer({
-  Image: cfg.imagename,
-  "Env": ["APP_NAME=" + cfg.appname, "HOST_PORT=" + HostPort]
-}, function(err, container) {
-	console.log("Host port : " + HostPort);
-	  container.start({
-	  	"ExposedPorts": {"3000/tcp": {}},
-	  	//uncomment if we want to provide host port instead of dynamically allocate
-		"PortBindings": {"3000/tcp":   [{}]}  //[{"HostPort": HostPort.toString()}]} //
-	  }, function(err, data) {
-	  	console.log('Container started : ' + data);
-	    runExec(container);
-	  });
-});
 
